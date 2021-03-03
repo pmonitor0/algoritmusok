71c2db90e121be9b9f0f07b51c9f765bd8ff82dc
pmonitor0 <79897131+pmonitor0@users.noreply.github.com>
2021-03-03 17:34:27 +0100

    Create permism.cs
    Ismétléses permutáció

---
+++ permutacio/permism.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Diagnostics;
+
+namespace Permutaciok
+{
+    class Permutal
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+            char[] charr = "abcdanananaabcdanana".ToCharArray();
+            Stopwatch sw = new Stopwatch();
+            long t_1 = 0;
+            sw.Start();
+            Teszt(charr);
+            t_1 = sw.ElapsedMilliseconds;
+            Console.WriteLine("Teszt: {0}", t_1);
+        }
+
+        static void Teszt(char[] arr)
+        {
+            char[] arr2 = (char[])arr.Clone();
+            int size = arr.Length;
+            Array.Sort(arr2);
+            int n = arr2.Length;
+            int i, j;
+            char temp;
+            while (true)
+            {
+                /*for (i = 0; i < n; ++i) Console.Write("{0} ", arr2[i]);
+                Console.WriteLine("");*/
+
+                for (i = n - 2; i >= 0 && arr2[i] >= arr2[i + 1]; --i) ;
+                if (i < 0) break;
+                for (j = n - 1; arr2[j] <= arr2[i]; --j) ;
+                temp = arr2[i]; arr2[i] = arr2[j]; arr2[j] = temp;
+                for (j = i + 1; j < n + i - j; ++j)
+                {
+                    temp = arr2[j]; arr2[j] = arr2[n + i - j]; arr2[n + i - j] = temp;
+                }
+            }
+        }
+    }
+}
