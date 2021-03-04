05779fc0fa1183132e4989095c4fe4ca2561b826
pmonitor0 <79897131+pmonitor0@users.noreply.github.com>
2021-03-04 07:57:04 +0100

    Update komb.cs
    Kombináció.

--- kombinacio/komb.cs
+++ kombinacio/komb.cs
@@ -13,14 +13,17 @@ namespace Kombinaciok
             int[] arr = new int[n];
             for (int i = 0; i < n; ++i) arr[i] = i + 1;
             Stopwatch sw = new Stopwatch();
-            long t_1 = 0;
+            long t_1 = 0, t_2 = 0, t3 = 0;
             sw.Start();
-            Kombinacio(arr, k);
+            Teszt_1(arr, k);
             t_1 = sw.ElapsedMilliseconds;
-            Console.WriteLine("Eltelt idő: {0}", t_1);
+            Teszt_2(arr, k);
+            t_2 = sw.ElapsedMilliseconds;
+            Console.WriteLine("Teszt_1: {0}", t_1);
+            Console.WriteLine("Teszt_2: {0}", t_2 - t_1);
         }
 
-        static void Kombinacio(int[] arr, int k)
+        static void Teszt_1(int[] arr, int k)
         {
             int n = arr.Length;
             int[] tomb = new int[k];
@@ -28,9 +31,7 @@ namespace Kombinaciok
             int j = k - 1;
             do
             {
-
-                /*++db;
-                for (int i = 0; i < k; ++i) Console.Write("{0} ", arr[tomb[i]]);
+                /*for (int i = 0; i < k; ++i) Console.Write("{0} ", arr[tomb[i]]);
                 Console.WriteLine("");*/
 
                 ++tomb[j];
@@ -55,6 +56,54 @@ namespace Kombinaciok
                     --j;
                 }
             } while (true);
+        }
+
+        static void Teszt_2(int[] arr, int k)
+        {
+            int n = arr.Length;
+            int[] tomb = new int[k];
+            int j = k - 1;
+            bool ind = true;
+            while(Kombinacio(tomb, n, k, ref j, ref ind))
+            {
+                /*for (int i = 0; i < k; ++i) Console.Write(arr[tomb[i]]);
+                Console.WriteLine("");*/
+            }
+        }
+
+        static bool Kombinacio(int[] tomb, int n, int k, ref int j, ref bool ind)
+        {
+            if(ind)
+            {
+                for (int i = 0; i < k; ++i) tomb[i] = i;
+                ind = false;
+                return true;
+            }
+            else
+            {
+                ++tomb[j];
+                if (tomb[j] > n - 1)
+                {
+                    while (j > -1)
+                    {
+                        if (tomb[j] >= n - k + j)
+                        {
+                            --j;
+                            if (j == -1) return false;
+                        }
+                        else break;
+                    }
+                    ++tomb[j];
+                    ++j;
+                    while (j < k)
+                    {
+                        tomb[j] = tomb[j - 1] + 1;
+                        ++j;
+                    }
+                    --j;
+                }
+                return true;
+            }
         }
     }
 }
